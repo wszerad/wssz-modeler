1de6dd95d9df5cd585335d98a0a1a1471df2b8f0
Wszerad <wszerad91@gmail.com>
2019-08-17 18:24:12 +0200

    more strict types

--- src/decorators.ts
+++ src/decorators.ts
@@ -13,6 +13,8 @@ export enum Formats {
 }
 
 type ExamplesType = {[key: string]: {value: any}};
+type BasicType = string | number | boolean | RegExp | Object;
+type BasicFunction = () => BasicType;
 
 export function Type(data?: any) {
 	const self = Type;
@@ -32,12 +34,12 @@ export const MaxLength = defineMarker<number>();
 export const MinLength = defineMarker<number>();
 export const Format = defineMarker<Formats>();
 export const Pattern = defineMarker<RegExp>();
-export const Enum = defineMarker<any[] | Object>();
+export const Enum = defineMarker<(string | number)[] | {[key: string]: string | number}>();
 export const MaxItems = defineMarker<number>();
 export const MinItems = defineMarker<number>();
-export const Default = defineMarker<any>();
-export const Example = defineMarker<any>();
-export const ArrayType = defineMarker<any>();
+export const Default = defineMarker<BasicType | BasicType[] | BasicFunction>();
+export const Example = defineMarker<BasicType>();
+export const ArrayType = defineMarker<Object>();
 export const UniqueItems = defineMarker<undefined>();
 export const Examples = defineMarker<ExamplesType>();
 export const Description = defineMarker<string>();
