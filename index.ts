ff5636d3e2a9c7a3e7856ef7dfefa0813692a8a0
Wszerad <wszerad91@gmail.com>
2019-09-09 22:46:16 +0200

    export ArrayItems

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export * from './src/decorators';
+export * from './src/ArrayItems';
 export {defineMarker, getMarkers, hasMarkers, extractDecoratorMarkers, Markers, PropMarkers} from './src/utils';
